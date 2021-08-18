47fe19dbb78dc2136de3c66b59f8f4de8456fa67
materiasipm <62041344+materiasipm@users.noreply.github.com>
2021-08-18 16:47:07 -0300

    Update ejemplo.js

--- taller4to/ejemplo.js
+++ taller4to/ejemplo.js
@@ -1,1 +1,17 @@
 console.log("hola de nuevo");
+
+function ejemplo1(nombreDeVariable) {
+    console.log("un número: " + nombreDeVariable);
+}
+
+function ejemplo2(nombreDeVariable) {
+    console.log("un string: " + nombreDeVariable);
+}
+
+function ejemplo3(nombreDeVariable) {
+    console.log("elemento html: " + nombreDeVariable);
+}
+
+function ejemplo4(nombreDeVariable) {
+    console.log("cualquier otro tipo de dato: " + nombreDeVariable);
+}
