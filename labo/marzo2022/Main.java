13c95b84f35877652500bd52389e3b103e1372b1
nadianoe <62041344+nadianoe@users.noreply.github.com>
2022-03-08 16:19:39 -0300

    Create Main.java

---
+++ labo/marzo2022/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    
+	public static void main(String[] args) {
+	    
+		System.out.println("Holaaaa");
+        
+		int a = 1;
+		float b = 23.55f;
+		String c = "Hola";
+		boolean d = true;
+		boolean e = false;
+		boolean f = true || false;
+		
+		if (d == e){
+		    System.out.println("Son números iguales");
+		} else {
+		    System.out.println("Son números distintos");
+		}
+		
+		Persona p1 = new Persona();
+		Persona p2 = new Persona("Lolo",45);
+		
+		p1.setEdad(88);
+		int edad = p1.getEdad()
+		boolean b;
+		
+	}
+	
+}
