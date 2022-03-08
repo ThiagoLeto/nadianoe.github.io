daaa0a617ec7049b73b7eae1ab3f38c1bcf3320f
nadianoe <62041344+nadianoe@users.noreply.github.com>
2022-03-08 16:20:12 -0300

    Create Persona.java

---
+++ labo/marzo2022/Persona.java
@@ -0,0 +1,32 @@
+
+public class Persona {
+    
+    private String nombre;
+    private int edad;
+    
+    public Persona(){
+        this.nombre = "Jaimito";
+        this.edad = 12;
+    }
+    
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    
+    public int getEdad(){
+        return this.edad;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    
+    public boolean esMayorDeEdad(){
+        return this.edad > 18;
+    }
+    
+    public boolean sonLaMismaPersona(Persona p){
+        /** hace algo ** /
+    }
+} 
