24345fb1d3bbf82dec706dfa3beaf9081ab55e92
nadianoe <62041344+nadianoe@users.noreply.github.com>
2022-03-09 12:04:03 -0300

    Create ejemplo10deMarzo.py

---
+++ taller6to2022/ejemplo10deMarzo.py
@@ -0,0 +1,46 @@
+
+if __name__ == "__main__":
+    
+    print("hola")
+    print("Hola","Holaaaa")
+    cadenas = "Holaaa" + "Hola" + 'Holaaaaaaa'
+    print(cadenas)
+    
+    num1 = 34
+    num2 = 2 
+    suma = num1 + num2
+    print(suma)
+    
+    num1 = 23.0
+    num2 = 0.5
+    print(num1 + num2)
+    
+    lista = [1,2,3,5,6,4,4,4,4]
+    lista = ["afdf",22,"hola",2,6.78]
+    print(lista)
+    
+    if (len(lista) == 2):
+        print("tiene dos elementos")
+    else:
+        print("tiene más de dos elementos")
+        
+    a = 1
+    b = 3
+    if b > a:
+      print("b es mayor")
+    elif a == b:
+      print("a y b son iguales")
+      
+  
+    for i in range(5):
+        print(i)
+ 
+    for elemento in lista:
+        print(elemento)
+        
+    while b > a:
+        print("abcd")
+        a = a + 1
+        
+        
+    
