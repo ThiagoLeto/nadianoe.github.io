b974de7a87edaa4034e16b26fd9f00b6e29bc995
nadianoe <62041344+nadianoe@users.noreply.github.com>
2022-03-09 11:33:20 -0300

    Update EjemploArrayList.java

--- labo/marzo2022/EjemploArrayList.java
+++ labo/marzo2022/EjemploArrayList.java
@@ -4,34 +4,34 @@ public class Main {
   
 	public static void main(String[] args) {
 		
-    / ** variables primitivas **/
+		/ ** variables primitivas **/
 		int a = 1;		
 		float c = 1.54f;
-    
-    /** Clases envolventes **/
-    Integer b = 1;
+
+		/** Clases envolventes **/
+		Integer b = 1;
 		Float d = 1.54f;
-		
-    
-    /** ArrayList **/
-    
+
+
+		/** ArrayList **/
+
 		ArrayList<Integer> nums = new ArrayList<>();
 		nums.add(1);
 		nums.add(5);
-		
+
 		int a = nums.get(0);
 		nums.remove(0);
-		
+
 		ArrayList<Persona> personas = new ArrayList<>();
-		
+
 		Persona p1 = new Persona();
-		
+
 		ArrayList<ArrayList<Integer>> matriz = new ArrayList<>();
     
-    /**
-    documentación:
-    https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
-    **/
-	
+	    /**
+	    documentación:
+	    https://docs.oracle.com/javase/8/docs/api/java/util/ArrayList.html
+	    **/
+
 	}
 }
