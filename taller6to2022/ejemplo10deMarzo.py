ac0c2449e1ab87818a26beb6a2e2f51376a52b82
nadianoe <62041344+nadianoe@users.noreply.github.com>
2022-03-09 23:37:25 -0300

    Update ejemplo10deMarzo.py

--- taller6to2022/ejemplo10deMarzo.py
+++ taller6to2022/ejemplo10deMarzo.py
@@ -1,46 +1,74 @@
 
-if __name__ == "__main__":
-    
+
+if __name__ == '__main__':
+
     print("hola")
-    print("Hola","Holaaaa")
+    print("Hola", "Holaaaa")
     cadenas = "Holaaa" + "Hola" + 'Holaaaaaaa'
     print(cadenas)
-    
+
     num1 = 34
-    num2 = 2 
+    num2 = 2
     suma = num1 + num2
     print(suma)
-    
+
     num1 = 23.0
     num2 = 0.5
     print(num1 + num2)
-    
-    lista = [1,2,3,5,6,4,4,4,4]
-    lista = ["afdf",22,"hola",2,6.78]
+
+    lista = [1, 2, 3, 5, 6, 4, 4, 4, 4]
+    lista = ["afdf", 22, "hola", 2, 6.78]
     print(lista)
-    
+
     if (len(lista) == 2):
         print("tiene dos elementos")
     else:
         print("tiene más de dos elementos")
-        
+
     a = 1
     b = 3
     if b > a:
-      print("b es mayor")
+        print("b es mayor")
     elif a == b:
-      print("a y b son iguales")
-      
-  
+        print("a y b son iguales")
+
     for i in range(5):
         print(i)
- 
+
     for elemento in lista:
         print(elemento)
-        
+
     while b > a:
         print("abcd")
         a = a + 1
-        
-        
-    
+
+    diccionario1 = {"Ana": 12, "Fabio": 3}
+    diccionario1["Lolo"] = 33
+    diccionario1['Lola'] = 90
+    valores = diccionario1.values()
+    valores = list(valores)
+
+    print(diccionario1)
+    print(valores[0])
+
+    set = {"Jazmín", "Karina"}
+    set.add("Paola")
+    print(set)
+
+    '''
+
+    documentación sobre listas:
+    https://python-reference.readthedocs.io/en/latest/docs/list/
+
+    documentación sobre conjuntos:
+    https://python-reference.readthedocs.io/en/latest/docs/sets/
+
+    documentación sobre diccionarios:
+    https://python-reference.readthedocs.io/en/latest/docs/dict/
+
+
+    '''
+
+
+
+
