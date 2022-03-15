90cec72338d26505c2ebc37a0d2c44ed6c969725
nadianoe <62041344+nadianoe@users.noreply.github.com>
2022-03-15 10:44:33 -0300

    Create ejemplo15deMarzo.cpp

---
+++ laboratorio4to/ejemplo15deMarzo.cpp
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    /* Sea A un conjunto tal que 
+       
+       A = (-∞,2) U [4,10]
+      
+    */
+    
+    int a = 5;
+    
+    
+    if(a < 2){
+        cout << "Es menor a 2. Pertenece a A."  << endl;
+    } else if (a >= 4 && a <= 10){
+        cout << "Está entre 4 y 10. Pertenece a A" << endl;
+    } else {
+        cout << "No pertenece a A."<< endl;
+    }
+    
+    a = a + 1;
+    a = a - 1;
+    a = 2 * a;
+    a = a + 2;
+    a = a + 1;
+    a++;
+    a = a - 1;
+    a--;
+    
+    int b,c,d;
+    
+    b = 1;
+    b++;
+    c = b;
+    cout << c << endl;
+    
+    
+    
+
+    return 0;
+}
+
+    
